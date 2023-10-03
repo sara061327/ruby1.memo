7fe25c26ac83316151de23049f991c57ac40dc10
EC2 Default User <ec2-user@ip-172-31-15-121.ap-southeast-2.compute.internal>
2023-10-03 14:48:29 +0000

    修正

--- memo.rb
+++ memo.rb
@@ -6,9 +6,9 @@ memo_type = gets.to_i
 
 if memo_type == 1
   puts "新規でメモを作成します。拡張子を除いたファイル名を入力してください。"
-  
-  file_name = gets.to_s
+ file_name = gets.to_s
   puts "メモの内容を入力してください。"
+  
   
   memo = STDIN.gets.to_s.chomp
   CSV.open("#{file_name}.csv" , "w") do |csv|
@@ -20,11 +20,14 @@ elsif memo_type == 2
   
   file_name = gets.to_s
   puts "メモの内容を入力してください。"
+  puts "Ctrl+Dで保存してください。"
+  puts "#{file_name}.csv"
   
-  memo = STDIN.gets.to_s.chomp
-  CSV.open("#{file_name}.csv" , "a") do |csv|
-    csv.puts ["#{memo}"]
-  end
+ memo = readlines.chomp.to_s
+   
+ CSV.open("{file_name}.csv" , "a") do |csv|
+ csv.puts ["#{memo}"]
+ end
   
 else
   puts"1か2の数字を入力してください"
